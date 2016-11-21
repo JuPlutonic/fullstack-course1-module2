360a4002d9c4a49b3daa910512923613db8d870a
0x639d1dec74 <guhtymailru@mailinator.com>
2016-11-21 14:05:51 +0100

    Added my comments

--- Lecture14-AccessControl/private_access.rb
+++ Lecture14-AccessControl/private_access.rb
@@ -1,12 +1,12 @@
 class Person
   def initialize(age)
-  	self.age = age # LEGAL - EXCEPTION
+  	self.age = age # LEGAL - EXCEPTION 'cause age comes from intializeng parameters, i.e. age here is a local variable (scope: method) 
   	puts my_age
   	# puts self.my_age # ILLEGAL
   	                   # CANNOT USE self or any other receiver
   end
 
-  private 
+  private  ########################
     def my_age
     	@age
     end

7e5b8774171852b9818ed7c428c6ca81239ba2a3
0x639d1dec74 <guhtymailru@mailinator.com>
2016-12-08 13:36:56 +0100

    WASTED, comments added

--- Assignments/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
+++ Assignments/Lesson03-Assignment01-Classes/student-start/module2_lesson3_formative.rb
@@ -1,7 +1,7 @@
 class Person
   #have a first_name and last_name attribute with public accessors
-  #attr_accessor 
-
+  #attr_accessor
+  attr_accessor :first_name, :last_name
   #have a class attribute called `people` that holds an array of objects
   @@people = []
 
@@ -9,18 +9,24 @@ class Person
   def initialize(x,y)#should take 2 parameters for first_name and last_name
     #assign those parameters to instance variables
     #add the created instance (self) to people class variable
+    self.first_name = x     ########NOT UNDERSTAND WHY FKG @@ NOT
+    self.last_name = y      ########WORKING
+    @@people << self     ####insrt the crtd instance (self) into the people class varbl
   end
 
   #have a `search` method to locate all people with a matching `last_name`
   def self.search(last_name)
     #accept a `last_name` parameter
     #search the `people` class attribute for instances with the same `last_name`
-    #return a collection of matching instances
+    #return a collection of matching instancesl
+    #    self.select {|last_name| @last_name === last_name}.to_s
+    @@people.select {|person| person.last_name === last_name} #######PERSON.LAST_NAME AAAAAAGGGRRRRHHHH!!!!
   end
 
   #have a `to_s` method to return a formatted string of the person's name
   def to_s
     #return a formatted string as `first_name(space)last_name`
+    "${first_name} ${last_name}"
   end
 end
 
